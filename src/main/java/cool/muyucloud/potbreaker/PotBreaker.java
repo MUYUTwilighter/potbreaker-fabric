73b71990c77959b44b1ed1011722f4db238dd49b
MUYU_Twilighter <1484605372@qq.com>
2023-11-14 15:08:43 +0800

    Fix mod main class

--- src/main/java/cool/muyucloud/potbreaker/PotBreaker.java
+++ src/main/java/cool/muyucloud/potbreaker/PotBreaker.java
@@ -2,13 +2,10 @@ package cool.muyucloud.potbreaker;
 
 import cool.muyucloud.tunnel.TunnelInitializer;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.text.TranslatableTextContent;
 
 public class PotBreaker implements ModInitializer {
     @Override
     public void onInitialize() {
-        Object
-        TranslatableTextContent content = new TranslatableTextContent("", "", );
         TunnelInitializer.init(PotBreaker.class);
     }
 }
