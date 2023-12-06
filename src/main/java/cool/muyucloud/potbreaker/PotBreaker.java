433a9b9046d64dc1b9476433d8da3aaacd349762
MUYU_Twilighter <1484605372@qq.com>
2023-12-06 20:11:13 +0800

    Fix environment

--- src/main/java/cool/muyucloud/potbreaker/PotBreaker.java
+++ src/main/java/cool/muyucloud/potbreaker/PotBreaker.java
@@ -2,6 +2,8 @@ package cool.muyucloud.potbreaker;
 
 import cool.muyucloud.tunnel.TunnelInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.mixin.dimension.EntityMixin;
+import net.fabricmc.fabric.mixin.event.lifecycle.WorldMixin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
